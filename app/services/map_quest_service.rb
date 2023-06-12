e9a6f6199b96019aa6f17c55ec39d38257783c9e
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-06-12 10:56:33 -0500

    Feat: add MapQuestService

---
+++ app/services/map_quest_service.rb
@@ -0,0 +1,21 @@
+class MapQuestService
+  BASE_URL = 'https://www.mapquestapi.com'.freeze
+
+  def get_city_lat_lon(city, state)
+    url = "/geocoding/v1/address?key=#{ENV['MAP_QUEST_API_KEY']}&location=#{city.capitalize},#{state.upcase}"
+    location_data = get_url(url)
+    location_data[:results].first[:locations].first[:latLng]
+  end
+
+  private
+
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(url: BASE_URL,
+      headers: { 'Content-Type' => 'application/json' })
+  end
+end
