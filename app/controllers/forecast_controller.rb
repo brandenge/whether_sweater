56a3e37a9a9a3193f945780fcb974a9dd8559b1f
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-06-12 16:22:38 -0500

    Fix: remove pry

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -1,6 +1,5 @@
 class ForecastController < ApplicationController
   def search
     coordinates = LocationFacade.new.get_city_lat_lon(params[:city], params[:state])
-    require 'pry-byebug'; require 'pry'; binding.pry;
   end
 end
