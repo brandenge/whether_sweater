680250f9f11af3f8d10a3707fc3ea4f962fe4699
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-06-13 12:58:17 -0500

    Test: add test for activity PORO

---
+++ spec/poros/activity_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Activity, type: :poros do
+  subject(:activity) { Activity.new(ActivityFixture::ATTRIBUTES) }
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(activity).to be_an(Activity)
+    end
+  end
+
+  describe 'attributes' do
+    it 'has attributes' do
+      expect(activity.activity).to eq('Do yoga')
+      expect(activity.type).to eq('recreational')
+      expect(activity.participants).to eq(1)
+      expect(activity.price).to eq(0)
+      expect(activity.link).to eq('')
+      expect(activity.key).to eq('4688012')
+      expect(activity.accessibility).to eq(0.9)
+    end
+  end
+end
