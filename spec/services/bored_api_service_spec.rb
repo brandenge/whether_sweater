195a93975718a37051b7e762c5c55a6c70ade5ba
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-06-13 12:35:14 -0500

    Test: add tests for Bored API Service

---
+++ spec/services/bored_api_service_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe BoredApiService, type: :services, vcr: { record: :new_episodes } do
+  describe 'instance methods' do
+    describe '#bored_api' do
+      it 'returns the latitude and longitude coordinates of a city' do
+        actual = BoredApiService.new.get_activities_for_weather(65)
+
+        expect(actual).to be_a(Array)
+        expect(actual.count).to eq(2)
+
+        actual.map do |activity|
+          expect(activity).to be_a(Hash)
+          expect(activity).to have_key(:activity)
+          expect(activity).to have_key(:type)
+          expect(activity).to have_key(:participants)
+          expect(activity).to have_key(:price)
+          expect(activity).to have_key(:link)
+          expect(activity).to have_key(:key)
+          expect(activity).to have_key(:accessibility)
+
+          expect(activity[:activity]).to be_a(String)
+          expect(activity[:type]).to be_a(String)
+          expect(activity[:participants]).to be_an(Integer)
+          expect(activity[:price]).to be_a(Numeric)
+          expect(activity[:link]).to be_a(String)
+          expect(activity[:key]).to be_a(String)
+          expect(activity[:accessibility]).to be_a(Numeric)
+        end
+      end
+    end
+  end
+end
