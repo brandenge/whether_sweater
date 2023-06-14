cdfccaafc20b28b16096d3e85752df5d2c8db4b5
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-06-13 19:22:09 -0500

    Fix: remove pry

--- app/controllers/api/v0/forecast_controller.rb
+++ app/controllers/api/v0/forecast_controller.rb
@@ -2,7 +2,7 @@ class Api::V0::ForecastController < ApplicationController
   def search
     coordinates = LocationFacade.new.get_city_lat_lng(*params[:location].split(','))
     weather_data = WeatherForecastFacade.new.get_forecast(coordinates)
-    require 'pry-byebug'; require 'pry'; binding.pry;
+
     render json: WeatherForecastSerializer.new(weather_data).serializable_hash.to_json, status: 200
   end
 end
