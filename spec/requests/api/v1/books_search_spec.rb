f3c866950a22ad9e978f07659320aba78985f226
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-06-13 20:47:09 -0500

    Test: add tests for key counts to ensure no extra data is sent

--- spec/requests/api/v1/books_search_spec.rb
+++ spec/requests/api/v1/books_search_spec.rb
@@ -10,8 +10,11 @@ RSpec.describe 'Books Search', type: :request, vcr: { record: :new_episodes } do
     books = JSON.parse(response.body, symbolize_names: true)
 
     expect(books).to be_a(Hash)
+    expect(books.keys.count).to eq(1)
     expect(books).to have_key(:data)
 
+    expect(books[:data]).to be_a(Hash)
+    expect(books[:data].keys.count).to eq(3)
     expect(books[:data]).to have_key(:id)
     expect(books[:data]).to have_key(:type)
     expect(books[:data]).to have_key(:attributes)
@@ -19,6 +22,7 @@ RSpec.describe 'Books Search', type: :request, vcr: { record: :new_episodes } do
     expect(books[:data][:id]).to eq(nil)
     expect(books[:data][:type]).to eq('books')
     expect(books[:data][:attributes]).to be_a(Hash)
+    expect(books[:data][:attributes].keys.count).to eq(4)
 
     expect(books[:data][:attributes]).to have_key(:destination)
     expect(books[:data][:attributes]).to have_key(:forecast)
@@ -26,10 +30,11 @@ RSpec.describe 'Books Search', type: :request, vcr: { record: :new_episodes } do
     expect(books[:data][:attributes]).to have_key(:books)
 
     expect(books[:data][:attributes][:destination]).to be_a(String)
-    expect(books[:data][:attributes][:forecast]).to be_a(Hash)
     expect(books[:data][:attributes][:total_books_found]).to be_a(Integer)
     expect(books[:data][:attributes][:books]).to be_an(Array)
 
+    expect(books[:data][:attributes][:forecast]).to be_a(Hash)
+    expect(books[:data][:attributes][:forecast].keys.count).to eq(2)
     expect(books[:data][:attributes][:forecast]).to have_key(:summary)
     expect(books[:data][:attributes][:forecast]).to have_key(:temperature)
 
@@ -38,6 +43,7 @@ RSpec.describe 'Books Search', type: :request, vcr: { record: :new_episodes } do
 
     books[:data][:attributes][:books].each do |book|
       expect(book).to be_a(Hash)
+      expect(book.keys.count).to eq(3)
       expect(book).to have_key(:isbn)
       expect(book).to have_key(:title)
       expect(book).to have_key(:publisher)
