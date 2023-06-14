1c16d45f87842710b22f6eb407ce5612f97fd9b6
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-06-14 00:45:14 -0500

    Test: add tests for factory

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,63 +1,100 @@
 FactoryBot.define do
+  current_temperature = Faker::Number.between(from: 0, to: 90)
+  current_date = Faker::Time.between(from: DateTime.now - 10, to: DateTime.now)
+
+  factory :current_weather do
+    skip_create
+
+    last_updated { current_date.strftime('%Y-%m-%d %H:%M') }
+    temperature { current_temperature }
+    feels_like {
+      Faker::Number.between(from: current_temperature - 10, to: current_temperature + 10)
+    }
+    humidity { Faker::Number.between(from: 1, to: 100) }
+    uvi { Faker::Number.between(from: 1, to: 11) }
+    visibility { Faker::Number.between(from: 20, to: 50) }
+    condition { ['Sunny', 'Rain', 'Snow', 'Partly Cloudy'].sample }
+    icon { ['sunny.png', 'rain.png', 'snow.png', 'partly-cloudy.png'].sample }
+
+    initialize_with { new(attributes) }
+  end
+
+  factory :daily_weather do
+    skip_create
+
+    date { (current_date).strftime('%Y-%m-%d %H:%M') }
+    sunrise { '07:15 AM' }
+    sunset { '08:15 PM' }
+    max_temp { current_temperature + 15 }
+    min_temp { current_temperature - 15 }
+    condition { ['Sunny', 'Rain', 'Snow', 'Partly Cloudy'].sample }
+    icon { ['sunny.png', 'rain.png', 'snow.png', 'partly-cloudy.png'].sample }
+
+    initialize_with { new(attributes) }
+  end
+
+  factory :hourly_weather do
+    skip_create
+
+    current_hour = Faker::Number.between(from: 0, to: 23)
+
+    time { "#{'%02d' % current_hour}:00" }
+    temperature { Faker::Number.between(from: current_temperature - 5, to: current_temperature + 5) }
+    condition { ['Sunny', 'Rain', 'Snow', 'Partly Cloudy'].sample }
+    icon { ['sunny.png', 'rain.png', 'snow.png', 'partly-cloudy.png'].sample }
+
+    initialize_with { new(attributes) }
+  end
+
   factory :weather_forecast do
     skip_create
 
-    current_date = Faker::Time.between(from: DateTime.now - 10, to: DateTime.now)
-    current_temperature = Faker::Number.between(from: 0, to: 90)
-
-    factory :current_weather do
-      skip_create
-
-      last_updated { current_date.strftime('%Y-%m-%d %H:%M') }
-      temperature { current_temperature }
-      feels_like {
-        Faker::Number.between(from: current_temperature - 10, to: current_temperature + 10)
+    current_weather do
+      current_weather = create(:current_weather)
+      {
+        last_updated: current_weather.last_updated,
+        temperature: current_weather.temperature,
+        feels_like: current_weather.feels_like,
+        humidity: current_weather.humidity,
+        uvi: current_weather.uvi,
+        visibility: current_weather.visibility,
+        condition: current_weather.condition,
+        icon: current_weather.icon
       }
-      humidity { Faker::Number.between(from: 1, to: 100) }
-      uvi { Faker::Number.between(from: 1, to: 11) }
-      visibility { Faker::Number.between(from: 20, to: 50) }
-      condition { ['Sunny', 'Rain', 'Snow', 'Partly Cloudy'].sample }
-      icon { condition.downcase.delete('-').delete('.png') }
-
-      initialize_with { new(attributes) }
     end
 
-    factory :daily_weather do
-      skip_create
-
-      date { (current_date).strftime('%Y-%m-%d %H:%M') }
-      sunrise { '07:15 AM' }
-      max_temp { current_temperature + 15 }
-      min_temp { current_temperature - 15 }
-      condition { ['Sunny', 'Rain', 'Snow', 'Partly Cloudy'].sample }
-      icon { ['sunny.png', 'rain.png', 'snow.png', 'partly-cloudy.png'].sample }
-
-      initialize_with { new(attributes) }
-    end
-
-    factory :hourly_forecast do
-      skip_create
-
-      time { "00:00" }
-      temperature { Faker::Number.between(from: current_temperature - 5, to: current_temperature + 5) }
-      condition { ['Sunny', 'Rain', 'Snow', 'Partly Cloudy'].sample }
-      icon { ['sunny.png', 'rain.png', 'snow.png', 'partly-cloudy.png'].sample }
-
-      initialize_with { new(attributes) }
-    end
-
-    current_weather { create(:current_weather) }
-    daily_weather {
-      create_list(:daily_weather, 5) do |daily_weather, index|
-        daily_weather(date: (current_date + 1 + index).strftime('%Y-%m-%d %H:%M'))
+    daily_weather do
+      daily_weather = create_list(:daily_weather, 5)
+      daily_weather.map do |day|
+        {
+          date: day.date,
+          sunrise: day.sunrise,
+          sunset: day.sunset,
+          max_temp: day.max_temp,
+          min_temp: day.min_temp,
+          condition: day.condition,
+          icon: day.icon
+        }
       end
-    }
-    hourly_weather {
-      create_list(:hourly_weather, 24) do |hourly_weather, index|
-        hourly_weather(time: "#{'%02d' % index}:00" )
+    end
+
+    hourly_weather do
+      hourly_weather = create_list(:hourly_weather, 24)
+      hourly_weather.map do |hour|
+        {
+          time: hour.time,
+          temperature: hour.temperature,
+          condition: hour.condition,
+          icon: hour.icon
+        }
       end
-    }
+    end
+
+    initialize_with do
+      new(current_weather: current_weather,
+            daily_weather: daily_weather,
+           hourly_weather: hourly_weather
+      )
+    end
   end
-
-  initialize_with { new(attributes) }
 end
---
+++ spec/poros/weather_forecast_factory_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+RSpec.describe WeatherForecast, type: :poros do
+  subject(:weather_forecast) { create(:weather_forecast) }
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(weather_forecast).to be_a(WeatherForecast)
+    end
+  end
+
+  describe 'attributes' do
+    it 'has attributes' do
+      expect(weather_forecast).to be_a(WeatherForecast)
+
+      expect(weather_forecast.id).to eq(nil)
+      expect(weather_forecast.type).to eq('forecast')
+
+      expect(weather_forecast.current_weather.last_updated)
+        .to be_a(String)
+      expect(weather_forecast.current_weather.temperature)
+        .to be_a(Numeric)
+      expect(weather_forecast.current_weather.feels_like)
+        .to be_a(Numeric)
+      expect(weather_forecast.current_weather.humidity)
+        .to be_a(Numeric)
+      expect(weather_forecast.current_weather.uvi)
+        .to be_a(Numeric)
+      expect(weather_forecast.current_weather.visibility)
+        .to be_a(Numeric)
+      expect(weather_forecast.current_weather.condition)
+        .to be_a(String)
+      expect(weather_forecast.current_weather.icon)
+        .to be_a(String)
+
+      expect(weather_forecast.daily_weather.count).to eq(5)
+      expect(weather_forecast.daily_weather.first.date)
+        .to be_a(String)
+      expect(weather_forecast.daily_weather.first.sunrise)
+        .to be_a(String)
+      expect(weather_forecast.daily_weather.first.sunset)
+        .to be_a(String)
+      expect(weather_forecast.daily_weather.first.max_temp)
+        .to be_a(Numeric)
+      expect(weather_forecast.daily_weather.first.min_temp)
+        .to be_a(Numeric)
+      expect(weather_forecast.daily_weather.first.condition)
+        .to be_a(String)
+      expect(weather_forecast.daily_weather.first.icon)
+        .to be_a(String)
+
+      expect(weather_forecast.hourly_weather.count)
+        .to be_a(Numeric)
+      expect(weather_forecast.hourly_weather.first.time)
+        .to be_a(String)
+      expect(weather_forecast.hourly_weather.first.temperature)
+        .to be_a(Numeric)
+      expect(weather_forecast.hourly_weather.first.condition)
+        .to be_a(String)
+      expect(weather_forecast.hourly_weather.first.icon)
+        .to be_a(String)
+    end
+  end
+end
