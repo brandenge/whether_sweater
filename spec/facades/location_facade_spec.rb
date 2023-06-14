945d787cc2c4d3bc6e80964a63f0a2a277fea289
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-06-13 23:12:43 -0500

    Fix: location facade sad path test

--- spec/facades/location_facade_spec.rb
+++ spec/facades/location_facade_spec.rb
@@ -1,27 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe LocationFacade, type: :facades, vcr: { record: :new_episodes } do
+  subject(:location_facade) { LocationFacade.new }
+
   describe 'instance methods' do
     describe '#get_city_lat_lng' do
       context 'Sad path - invalid city and state location' do
         it 'returns a custom error for an invalid location' do
-          expect { LocationFacade.new.get_city_lat_lng('#@$O@#(%)') }
+          expect { location_facade.get_city_lat_lng('#@$O@#(%)') }
             .to raise_error(CustomError)
-            begin
-              LocationFacade.new.get_city_lat_lng('Ggeim,FG')
-            rescue CustomError => e
-              expect(e.message).to eq('Invalid location. Please provide a valid city and state location.')
-              expect(e.status).to eq(400)
-            end
+          begin
+            location_facade.get_city_lat_lng('#@$O@#(%)')
+          rescue CustomError => e
+            expect(e.message).to eq('Invalid location. Please provide a valid city and state location.')
+            expect(e.status).to eq(400)
+          end
         end
       end
 
       context 'Sad path - city and state location not found' do
         it 'returns a custom error for a location not found' do
-          expect { LocationFacade.new.get_city_lat_lng('Ggeim,FG') }
+          expect { location_facade.get_city_lat_lng('Ggeim,FG') }
             .to raise_error(CustomError)
           begin
-            LocationFacade.new.get_city_lat_lng('Ggeim,FG')
+            location_facade.get_city_lat_lng('Ggeim,FG')
           rescue CustomError => e
             expect(e.message).to eq('No location found. Please provide a known location query parameter.')
             expect(e.status).to eq(400)
@@ -35,7 +37,7 @@ RSpec.describe LocationFacade, type: :facades, vcr: { record: :new_episodes } do
             lat: 38.89037,
             lng: -77.03196
           }
-          expect(LocationFacade.new.get_city_lat_lng('washington,dc'))
+          expect(location_facade.get_city_lat_lng('washington,dc'))
             .to eq(expected)
         end
       end
