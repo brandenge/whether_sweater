d5c540b7e0c614ab66e2383de2661f536a0385b2
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-06-14 06:05:44 -0500

    Test: add sad path test for map quest service method

--- spec/services/map_quest_service_spec.rb
+++ spec/services/map_quest_service_spec.rb
@@ -5,13 +5,28 @@ RSpec.describe MapQuestService, vcr: { record: :new_episodes } do
 
   describe 'instance methods' do
     describe '#get_city_lat_lon' do
-      it "returns the latitude and longitude coordinates of a city" do
-        expected = {
-          lat: 39.74001,
-          lng: -104.99202
-        }
-        expect(map_quest_service.get_city_lat_lng('denver,co'))
-          .to eq(expected)
+      context 'Happy path - valid and known location' do
+        it "returns the latitude and longitude coordinates of a city" do
+          expected = {
+            lat: 39.74001,
+            lng: -104.99202
+          }
+          expect(map_quest_service.get_city_lat_lng('denver,co'))
+            .to eq(expected)
+        end
+      end
+
+      context 'Sad path - invalid or unknown location' do
+        it 'returns an error message for invalid or unknown location' do
+          expect { map_quest_service.get_city_lat_lng('Ggeim,FG') }
+            .to raise_error(CustomError)
+          begin
+            map_quest_service.get_city_lat_lng('Ggeim,FG')
+          rescue CustomError => e
+            expect(e.message).to eq('No location found. Please provide a known location query parameter.')
+            expect(e.status).to eq(400)
+          end
+        end
       end
     end
   end
